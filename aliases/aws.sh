9f98222ee0e887252495bef4fc9ea81295c82601
Ramazan KORKMAZ <rakodev@gmail.com>
2021-10-29 22:50:35 +0200

    add few aliases

--- aliases/aws.sh
+++ aliases/aws.sh
@@ -4,6 +4,8 @@
 alias aws-edit-credentials='vim ~/.aws/credentials'
 alias aws-edit-config='vim ~/.aws/config'
 alias aws-list-profile='aws configure list-profiles'
+# Validate a sam template file, you can also specify the template file location as a parameter
+alias sam-validate='sam validate'
 
 # aws-ssh-ec2 EC2Tutorial.pem 52.51.204.186
 aws-ssh-ec2() {
@@ -36,4 +38,13 @@ aws-copy-to-s3(){
 		return 0;
 	fi
 	aws s3 cp $1 s3://$2
+}
+
+# it shows you which status is currently your stack
+aws-cloudformation-stack-status(){
+	if [ -z "$1" ]; then
+		echo "StackName is missing!";
+		return 0;
+	fi
+	aws cloudformation describe-stacks --stack-name $1 --query "Stacks[0].StackStatus" --output text
 }
--- aliases/git.sh
+++ aliases/git.sh
@@ -21,6 +21,10 @@ alias git-file-history='git log -p' # <file path>
 alias git-log='git log --graph --oneline --decorate'
 alias git-switch-to-previous-branch='git checkout -'
 alias git-commit='git commit -m' # "commit description"
+alias git-add-to-previous-commit='git commit --amend --no-edit'
+alias git-log-changes='git log -p'
+alias git-log-list='git log --pretty="%ar - %h - %an - %s"'
+alias git-commit-amend='git-add-patch & git commit --amend' # add new edit to the last unpushed commit
 
 # use it like this git-edit-last-pushed-commit 5054777864e4345d115f80025f913a403381d6c8 "my commit message I want"
 git-edit-last-pushed-commit() {
